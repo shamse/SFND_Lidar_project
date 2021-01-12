ce22145b603a2761618699336518760cde86e388
shamse <ehsan.shams@gmail.com>
2021-01-11 16:05:19 -0800

    created lidar point cloud

--- src/environment.cpp
+++ src/environment.cpp
@@ -46,9 +46,14 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
     // TODO:: Create lidar sensor 
+    Lidar* lidar = new Lidar(cars, 0.0);
 
     // TODO:: Create point processor
+    auto point_cloud {lidar->scan()};
+    renderRays(viewer, (Vect3{0,0,0}), point_cloud);
   
+
+    delete lidar;
 }
 
 
